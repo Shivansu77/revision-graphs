a26e2b3a5f628cd7180df836fef0ce52952b1951
shivansu77 <shivansubisht77@gmail.com>
2025-11-01 22:37:14 +0530

    loop.java

---
+++ graphs/loop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class loop {
+    public static void main(String[] args) {
+       int n = Integer.MAX_VALUE;
+       for(int i = 0; i < n; i++) {
+           System.out.println("hello");
+       } 
+    }
+}
